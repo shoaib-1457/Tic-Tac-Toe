a6d7846c1335b8571e7b1ccc27c338222fc32914
Adil Parwez <dlprwz@gmail.com>
2023-10-23 19:23:14 +0530

    Project Architecture Fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
+let music = new Audio("./assets/media/music.mp3")
+let audioTurn = new Audio("./assets/media/ting.mp3")
+let gameover = new Audio("./assets/media/gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
